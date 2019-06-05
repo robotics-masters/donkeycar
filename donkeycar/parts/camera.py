af728a932f41ed562b451844c272779be38cc442
wallarug (ardupilot) <wallarug@coliemore.com.au>
2019-06-05 23:14:49 +1000

    added jetson nano camera from @feicccccccc

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -10,6 +10,39 @@ class BaseCamera:
     def run_threaded(self):
         return self.frame
 
+class CSICamera(BaseCamera):
+    def gstreamer_pipeline(self,capture_width=120, capture_height=160, display_width=120, display_height=160, framerate=20, flip_method=0) :   
+        return ('nvarguscamerasrc ! ' 
+        'video/x-raw(memory:NVMM), '
+        'width=(int)%d, height=(int)%d, '
+        'format=(string)NV12, framerate=(fraction)%d/1 ! '
+        'nvvidconv flip-method=%d ! '
+        'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+        'videoconvert ! '
+        'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+    
+    def __init__(self, resolution=(120, 160), framerate=60):
+        import cv2
+        # initialize the camera and stream
+        self.camera = cv2.VideoCapture(self.gstreamer_pipeline(display_width=resolution[1],display_height=resolution[0],flip_method=0), cv2.CAP_GSTREAMER)
+        self.ret , self.image = self.camera.read()
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+
+        print('CSICamera loaded.. .warming camera')
+        time.sleep(2)
+
+    def run(self):
+        ret, frame = self.camera.read()
+        return frame
+    
+    def shutdown(self):
+        # indicate that the thread should be stopped
+        print('stoping CSICamera')
+        time.sleep(.5)
+        del(self.camera)
+
 
 class PiCamera(BaseCamera):
     def __init__(self, resolution=(120, 160), framerate=20):
