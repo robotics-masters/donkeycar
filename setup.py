e25695ee841c816764fdfb49f3823752e8df6c57
wallarug (ardupilot) <wallarug@coliemore.com.au>
2019-06-05 23:30:04 +1000

    added support for Jetson Nano install through donkeycar[nano]

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 
 
 setup(name='donkeycar',
-      version='2.5.7',
+      version='2.7.0a',
       description='Self driving library for python.',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/autorope/donkeycar',
-      download_url='https://github.com/autorope/donkeycar/archive/2.1.5.tar.gz',
-      author='Will Roscoe',
-      author_email='wroscoe@gmail.com',
+      url='https://github.com/robotics-masters/donkeycar',
+      download_url='https://github.com/robotics-masters/donkeycar/archive/2.7.0a.tar.gz',
+      author='Walla',
+      author_email='wallarug@github.com',
       license='MIT',
       entry_points={
           'console_scripts': [
@@ -40,6 +40,16 @@ setup(name='donkeycar',
                       'pi': [
                           'picamera',
                           'Adafruit_PCA9685',
+                          'serial',
+                          'Adafruit-Blinka',
+                          'adafruit-circuitpython-busdevice',
+                          'Adafruit_CircuitPython_seesaw',
+                          ],
+                      'jetson' : [
+                          'serial',
+                          'Adafruit_PCA9685',
+                          'keras',
+                          'scipy',
                           ],
                       'dev': [
                           'pytest',
