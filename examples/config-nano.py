9cf85589d28b1db284fd252c2837e34679171922
wallarug (Jetson Nano) <wallarug@coliemore.com.au>
2019-06-10 20:45:33 +0800

    added jetson nano sample config.py

---
+++ examples/config-nano.py
@@ -0,0 +1,52 @@
+"""
+CAR CONFIG
+
+This file is read by your car application's manage.py script to change the car
+performance.
+
+EXMAPLE
+-----------
+import dk
+cfg = dk.load_config(config_path='~/mycar/config.py')
+print(cfg.CAMERA_RESOLUTION)
+
+"""
+
+
+import os
+
+#PATHS
+CAR_PATH = PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(CAR_PATH, 'data')
+MODELS_PATH = os.path.join(CAR_PATH, 'models')
+
+#VEHICLE
+DRIVE_LOOP_HZ = 20
+MAX_LOOPS = 100000
+ACTUATOR_MODE='serial'
+USE_JOYSTICK_AS_DEFAULT=False
+
+#CAMERA
+CAMERA_RESOLUTION = (120, 160) #(height, width)
+CAMERA_FRAMERATE = DRIVE_LOOP_HZ
+
+#STEERING
+STEERING_CHANNEL = 1
+STEERING_LEFT_PWM = 1000
+STEERING_RIGHT_PWM = 2000
+
+#THROTTLE
+THROTTLE_CHANNEL = 0
+THROTTLE_FORWARD_PWM = 2000
+THROTTLE_STOPPED_PWM = 1500
+THROTTLE_REVERSE_PWM = 1000
+
+#TRAINING
+BATCH_SIZE = 128
+TRAIN_TEST_SPLIT = 0.8
+
+
+TUB_PATH = os.path.join(CAR_PATH, 'tub') # if using a single tub
+
+#ROPE.DONKEYCAR.COM
+ROPE_TOKEN="GET A TOKEN AT ROPE.DONKEYCAR.COM"
